3bbb514f7ac427c26021da6cf90430a7340d15ff
Stephen Lawrence <stephen@Stephens-MacBook-Pro.local>
2022-08-21 14:58:53 -0700

    display last scanned barcode

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -3,95 +3,77 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
+    @override
+    _MyAppState createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-  String _scanBarcode = 'Unknown';
 
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  Future<void> startBarcodeScanStream() async {
-    FlutterBarcodeScanner.getBarcodeStreamReceiver(
-            '#ff6666', 'Cancel', true, ScanMode.BARCODE)!
-        .listen((barcode) => print(barcode));
-  }
-
-  Future<void> scanQR() async {
-    String barcodeScanRes;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    try {
-      barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
-          '#ff6666', 'Cancel', true, ScanMode.QR);
-      print(barcodeScanRes);
-    } on PlatformException {
-      barcodeScanRes = 'Failed to get platform version.';
+    @override
+    void initState() {
+        super.initState();
+        startScanner();
     }
 
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
+    String barcode = 'welcome';
+    var scanner = new StreamController<String>();
 
-    setState(() {
-      _scanBarcode = barcodeScanRes;
-    });
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> scanBarcodeNormal() async {
-    String barcodeScanRes;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    try {
-      barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
-          '#ff6666', 'Cancel', true, ScanMode.BARCODE);
-      print(barcodeScanRes);
-    } on PlatformException {
-      barcodeScanRes = 'Failed to get platform version.';
+    void readBarcode(reading) {
+        if((reading != -1) && (reading != barcode)) {
+            if(reading != '-1') {
+                barcode = reading as String;
+                scanner.add(barcode);
+                HapticFeedback.lightImpact();
+            }
+        }
     }
 
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
+    Future<void> startScanner() async {
+        final stream = FlutterBarcodeScanner.getBarcodeStreamReceiver(
+            '#FF4CAF50','Details',true,ScanMode.BARCODE);
+        final subscription = stream!.listen(readBarcode);
+    }
 
-    setState(() {
-      _scanBarcode = barcodeScanRes;
-    });
-  }
+    Widget getDetails(String id) {
+        if(id == 'welcome') return Text(id);
+        var content = 'none';
+        // var url = Uri.parse('https://world.openfoodfacts.org/api/v0/product/$id.json');
+        // var response = http.get(url);
+        // if (response.statusCode == 200) content = jsonDecode(response);
+        // print(content);
+        return Text(content);
+    }
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(title: const Text('Barcode scan')),
-            body: Builder(builder: (BuildContext context) {
-              return Container(
-                  alignment: Alignment.center,
-                  child: Flex(
-                      direction: Axis.vertical,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        ElevatedButton(
-                            onPressed: () => scanBarcodeNormal(),
-                            child: Text('Start barcode scan')),
-                        ElevatedButton(
-                            onPressed: () => scanQR(),
-                            child: Text('Start QR scan')),
-                        ElevatedButton(
-                            onPressed: () => startBarcodeScanStream(),
-                            child: Text('Start barcode scan stream')),
-                        Text('Scan result : $_scanBarcode\n',
-                            style: TextStyle(fontSize: 20))
-                      ]));
-            })));
-  }
+    @override
+    Widget build(BuildContext context) {
+        return MaterialApp(
+            title: "foodee",
+            theme: ThemeData(primarySwatch: Colors.green),
+            home: Scaffold(
+                appBar: AppBar(title: const Text('Product Details')),
+
+                body: StreamBuilder<String>(
+                    stream: scanner.stream,
+                    builder: (context, snapshot) {
+                        print(snapshot.toString());
+                        print(barcode);
+                        return getDetails(barcode);
+                    }
+                ),
+
+                floatingActionButton: FloatingActionButton.extended(
+                    onPressed: () => startScanner(),
+                    icon: const Icon(Icons.qr_code_scanner),
+                    label: const Text('Scan'),
+                ),
+                floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+            )
+        );
+    }
 }
